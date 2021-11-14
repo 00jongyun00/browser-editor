c7c0d833cf17e7eac54e81376b88b6f00a2ac034
dev@jongyun.com <dev@jongyun.com>
2021-11-14 14:58:59 +0900

    Generating the Unpkg URL

--- src/unpkg-path-plugin.ts
+++ src/unpkg-path-plugin.ts
@@ -14,12 +14,17 @@ export const unpkgPathPlugin = () => {
         console.log('onResolve', args);
         if (args.path === 'index.js') {
           return { path: args.path, namespace: 'a' };
-        } else {
-          return {
-            path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
-            namespace: 'a',
-          };
         }
+        return {
+          namespace: 'a',
+          path: `https://unpkg.com/${args.path}`,
+        };
+        // else {
+        //   return {
+        //     path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
+        //     namespace: 'a',
+        //   };
+        // }
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
