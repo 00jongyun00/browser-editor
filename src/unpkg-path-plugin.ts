9f12bc02baf234d191827371564529d258ed72dd
dev@jongyun.com <dev@jongyun.com>
2021-11-14 15:18:38 +0900

    Using the URL Constructor

--- src/unpkg-path-plugin.ts
+++ src/unpkg-path-plugin.ts
@@ -15,6 +15,14 @@ export const unpkgPathPlugin = () => {
         if (args.path === 'index.js') {
           return { path: args.path, namespace: 'a' };
         }
+
+        if (args.path.includes('./') || args.path.includes('../')) {
+          return {
+            namespace: 'a',
+            path: new URL(args.path, args.importer + '/').href,
+          };
+        }
+
         return {
           namespace: 'a',
           path: `https://unpkg.com/${args.path}`,
@@ -35,7 +43,7 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-                  const message = require('tiny-test-pkg');
+                  const message = require('medium-test-pkg');
                   console.log(message);
                 `,
           };
