fcc409e83698e843db34c400ab211f60991c1719
dev@jongyun.com <dev@jongyun.com>
2021-11-20 13:40:38 +0900

    Handling Editor Change Events

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -2,11 +2,19 @@ import MonacoEditor from '@monaco-editor/react';
 
 interface CodeEditorProps {
   initialValue: string;
+  onChange(value: string): void;
 }
 
-const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue }) => {
+const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
+  const onEditortDidMounnt = (getValue: () => string, monacoEditor: any) => {
+    monacoEditor.onDidChangeModelContent(() => {
+      onChange(getValue());
+    });
+  };
+
   return (
     <MonacoEditor
+      editorDidMount={onEditortDidMounnt}
       value={initialValue}
       height="500px"
       language="javascript"
--- src/index.tsx
+++ src/index.tsx
@@ -67,7 +67,10 @@ const App = () => {
 
   return (
     <div>
-      <CodeEditor initialValue="const a = 1;" />
+      <CodeEditor
+        initialValue="const a = 1;"
+        onChange={(value) => setInput(value)}
+      />
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}
