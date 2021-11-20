25eb8b0ef446289690b4af0d07c3c6069efcc974
dev@jongyun.com <dev@jongyun.com>
2021-11-20 13:23:37 +0900

    Setting the Initial Value

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,7 +1,23 @@
 import MonacoEditor from '@monaco-editor/react';
 
 const CodeEditor = () => {
-  return <MonacoEditor height="500px" language="javascript" theme="dark" />;
+  return (
+    <MonacoEditor
+      height="500px"
+      language="javascript"
+      theme="dark"
+      options={{
+        wordWrap: 'on',
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+      }}
+    />
+  );
 };
 
 export default CodeEditor;
